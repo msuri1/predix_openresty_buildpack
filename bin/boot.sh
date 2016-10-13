efc5fa7be61b476f390bb83dbcc2d00ea8836e3b
Tim Hutchinson <timothy.hutchinson@ge.com>
2016-10-13 13:59:05 -0400

    Wrapped nameservers in quotes for multiple entries

--- bin/boot.sh
+++ bin/boot.sh
@@ -35,7 +35,7 @@ erb $conf_file > $APP_ROOT/openresty/nginx/conf/nginx-final.conf
 
 # Set env variable for DNS servers
 echo $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)
-export NAMESERVERS=$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)
+export NAMESERVERS="$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)"
 
 # ------------------------------------------------------------------------------------------------
 
