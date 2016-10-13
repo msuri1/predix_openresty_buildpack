d652b8a9642a801552f75ff4229bf53da8a8d8b0
Tim Hutchinson <timothy.hutchinson@ge.com>
2016-10-13 14:12:43 -0400

    Added quotes

--- bin/boot.sh
+++ bin/boot.sh
@@ -44,6 +44,6 @@ touch $APP_ROOT/openresty/nginx/logs/access.log
 touch $APP_ROOT/openresty/nginx/logs/error.log
 
 (tail -f -n 0 $APP_ROOT/openresty/nginx/logs/*.log &)
-exec env NAMESERVERS=$NAMESERVERS $APP_ROOT/openresty/nginx/sbin/nginx -p $APP_ROOT/openresty/nginx -c $APP_ROOT/openresty/nginx/conf/nginx-final.conf
+exec env NAMESERVERS="$NAMESERVERS" $APP_ROOT/openresty/nginx/sbin/nginx -p $APP_ROOT/openresty/nginx -c $APP_ROOT/openresty/nginx/conf/nginx-final.conf
 
 # ------------------------------------------------------------------------------------------------
